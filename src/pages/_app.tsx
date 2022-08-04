9e2d66c37f3d63f3ba12b9896d60c88a291c056f
Dhruv Dave <tatvmasiproject@gmail.com>
2022-08-04 22:15:18 +0530

    GOOGLE_ANALYTICS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
-import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
+
+import Seo from '@/components/Seo';
 
 /**
  * !STARTERCONF info
@@ -12,9 +15,24 @@ import '@/styles/colors.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <Seo />
+      <ThemeProvider attribute='class'>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
